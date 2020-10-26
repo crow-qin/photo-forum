818260e28b8c884b32d5c1b35842d62f7c842624
hgy <1309843131@qq.com>
2020-10-26 18:18:00 +0800

    详情过渡

--- src/views/home/search/SearchDetail.vue
+++ src/views/home/search/SearchDetail.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="box-all">
     <Bar />
-    <div class="f-16 color-3 t-center" v-if="List.length==0">
+    <!-- <div class="f-16 color-3 t-center" v-if="List.length==0">
       找不到
+    </div> -->
+    <!-- <div v-else> -->
+    <div class="h-all">
+      <div class="slide-nav w-all f-aj-c">
+        <div class="slide-nav-item f-14 f1 t-center" :class="{'color-blue': slideIndex==i}" v-for="(v,i) in slideArr" :key="i" @click="selectedIndex(i)">
+          {{v}}
+        </div>
+        <div class="slide-nav-active" :style="slideSty"></div>
+      </div>
+      <transition name="translate">
+
+        <div class="w-all h-all" :key="slideIndex" :class="'bg-'+slideIndex">{{slideIndex}}</div>
+
+      </transition>
     </div>
-    <div v-else>详情</div>
   </div>
 </template>
 
@@ -20,8 +33,12 @@ import Bar from '@/components/Bar.vue'
 export default class SearchDetail extends Vue {
   private kw: string = ''
   private List: any[] = []
+  private slideArr: string[] = ['最热','作者','图片']
+  private slideIndex: number = 0
 
-
+  get slideSty(): string {
+    return `transform: translateX(${100*this.slideIndex}%)`
+  }
   mounted(): void {
     let {kw} = this.$route.query
     this.kw = kw as string
@@ -32,9 +49,54 @@ export default class SearchDetail extends Vue {
   getDetail() {
     
   }
+
+  selectedIndex(i:number) {
+    this.slideIndex = i
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.slide-nav {
+  height: 40px;
+  position: relative;
+  
+  &-item {
+    line-height: 40px;
+    
+  }
+  &-active {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 2px;
+    background: #0084ff;
+    width: calc(100% / 3);
+    transition: transform .6s;
+  }
+}
+.bg {
+  &-0 {
+    background: #f8dddd22;
+  }
+  &-1 {
+    background: #fdf58d22;
+  }
+  &-2 {
+    background: #8ddfdd22;
+  }
+}
+.translate-enter-active,
+ .translate-leave-active {
+  transition: transform .6s ease;
+}
+.translate-enter-to, .translate-leave {
+  transform: translateX(0);
+}
+.translate-enter {
+  transform: translateX(-100%);
+}
+ .translate-leave-to{
+  transform: translateX(100%);
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+// 代码压缩
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+// gzip压缩
+const CompressionPlugin = require('compression-webpack-plugin')
+
+// 是否为生产环境
+const isProduction = process.env.NODE_ENV !== 'development'
+ console.log(process.env.NODE_ENV)
 module.exports = {
   devServer: {
     // 热更新
@@ -44,6 +53,65 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  productionSourceMap : false,
+
+  configureWebpack: config=> {
+
+    if(isProduction) {
+      // 代码压缩
+      config.plugins.push(
+        new UglifyJsPlugin({
+          ugifyOptions: {
+            // 生产环境自动删除console
+            compress: {
+              warnings: false,
+              drop_debugger: true,
+              drop_console: true,
+              pure_func: ['console.log']
+            }
+          },
+        sourceMap: false,
+        parallel: true
+        })
+      )
+    }
+
+    // gzip压缩
+    const productionGzipExtensions = ['html', 'js', 'css']
+    config.plugins.push(
+      new CompressionPlugin({
+        filename: '[path],gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.('+ productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        // 大小大于该值的资源会被处理
+        minRatio: 0.8,
+        // 压缩率小于该值得资源会被处理
+        deleteOriginalAssets: false,
+        // 删除原文件 
+      })
+    )
+
+    // 公共代码抽离
+    config.optimization = {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: /node_module/,
+            name: 'vendor',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100
+          }
+        },
+        runtimeChunk: {
+          name: 'manifest'
+        }
+      }
     }
   }
 }
