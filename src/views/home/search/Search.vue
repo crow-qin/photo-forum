66fe502fac23c22a561066ee39dddbb7d4357408
hgy <1309843131@qq.com>
2020-11-18 18:20:06 +0800

    修改搜索

--- src/views/home/search/Search.vue
+++ src/views/home/search/Search.vue
@@ -3,13 +3,13 @@
     <Bar />
     <div class="f-a-c p-8">
       <input type="text" v-model="text" class="f1 ipt-box f-14" />
-      <div class="f-16 ipt-text t-center" @click="openPage()">搜索</div>
+      <div class="f-16 ipt-text t-center" @click="openPage('')">搜索</div>
     </div>
     <div class=" f-14 color-3 f-j-sb f-a-c f-wrap p-16" v-show="historyRecord.length>0">
-      <div>最近搜索</div>
+      <div class="m-b-8">最近搜索</div>
       <div @click="showDia('all')">清除全部</div>
-      <div class="w-all">
-        <div class="f-16 h-32" v-for="v in historyRecord" :key="v.date" @touchstart="onStart(v)" @touchmove="onMove(v)" @touchend="onEnd(v)">
+      <div class="w-all f-a-c f-wrap">
+        <div class="s-history-box-item f-16 h-32" v-for="v in historyRecord" :key="v.date" @touchstart="onStart(v)" @touchmove="onMove(v)" @touchend="onEnd(v)">
           {{v.text}}
         </div>
       </div>
@@ -26,7 +26,7 @@ interface HrItem {
   date: string
 }
 
-@Component({
+@Component({  
   components: {
     Bar
   },
@@ -41,13 +41,13 @@ export default class Search extends Vue {
   private openPage(flag: string = ''): void {
     console.log(flag);
     
-    if(flag == '') {
+    if(flag == ''&&this.text.length>0) {
       flag = this.text
-      this.addHR(this.text)
-    } else {
-      this.addHR(flag) 
     }
-    this.$router.push({name: 'search-detail',query: {kw: flag}})
+    
+    this.addHR(flag) 
+    
+    // this.$router.push({name: 'search-detail',query: {kw: flag}})
   }
   addHR(text: string): void {
     if(text.trim() == '') return 
@@ -74,7 +74,31 @@ export default class Search extends Vue {
   private showDia(flag: string | HrItem):void {
     let text: string = flag=='all' ? '是否删除所有历史记录' : '是否删除该条历史记录' 
     console.log(text);
-    this.delHR(flag)
+    this.$createDialog({
+      type: 'confirm',
+      content: text,
+       confirmBtn: {
+          text: '确定',
+          active: true,
+        },
+        cancelBtn: {
+          text: '取消',
+          active: false,
+        },
+        onConfirm: () => {
+          this.delHR(flag)
+        }
+        
+      },
+      // (createElement: any)=>{
+      //   return  createElement('p',{
+      //       'class': {
+      //         'p-t-16': true
+      //       },
+      //       slot: 'title'
+      //     }, text)
+      // }
+    ).show()
     
   }
   onStart(obj: HrItem): void {
@@ -122,6 +146,15 @@ export default class Search extends Vue {
     width: 60px;
     line-break: 44px;
     cursor: pointer;
+  }
+}
+.s-history {
+
+  &-box-item {
+    padding: 0 16px;
+    border-radius: 16px;
+    margin: 0 16px 16px 0;
+    background: rgba($color: #0084ff, $alpha: .1);
   }
 }
 </style>
